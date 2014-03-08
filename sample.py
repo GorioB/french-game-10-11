a23bfe6ff5e7387ed788872e1f7937908415712c
GorioB <gmcbuencamino@gmail.com>
2014-03-08 12:50:38 +0800

    Changed 'lib' folder to 'content'

--- sample.py
+++
@@ -1,8 +0,0 @@
-class node:
-	def __init__(self,name='',neighbors={}):
-		self.name=name
-		self.neighbors=neighbors
-
-	def addNeighbor(self,neighborTag,neighbor):
-		if neighborTag not in self.neighbors.keys():
-			self.neighbors[neighborTag] = neighbor
