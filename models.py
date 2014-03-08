777cde7b634b4365a065841db87d3dffcd139456
GorioB <gmcbuencamino@gmail.com>
2014-03-08 12:49:05 +0800

    Added templates for writing game content, and persons and objects go models

--- models.py
+++ models.py
@@ -21,21 +21,41 @@ class room:
 		return "ROOM "+ident
 
 	def lookAt(self):
-		return self.name+"\n"+self.desc
+		return self.name+"\n"+self.desc+"objects: "+self.objects
 
 	def addDoor(self,doorName,target,targetDoor=''):
 		if doorName not in self.doors.keys():
 			self.doors.update({doorName:target})
-			if targetDoor=='':
-				if DOOR_COMPLIMENTS[doorName] not in self.doors[doorName].doors:
-					self.doors[doorName].doors[DOOR_COMPLIMENTS[doorName]] = self
+
+class item:
+	def __init__(self,ident='',name='',desc='',consumable=False,uses=None):
+		self.name=name
+		self.desc=desc
+		self.uses=uses
+		self.ident = ident
+		self.isConsumable=consumable
+		if self.uses=None:
+			self.uses={}
+
+class actor:
+	def __init__(self,ident='',name='',desc='',inventory=None,lines=None):
+		self.name=name
+		self.desc=desc
+		self.intentory=inventory
+		self.lines=lines
+		self.ident = ident
+		if self.inventory==None:
+			self.inventory={}
+
+		if self.lines==None:
+			self.lines={}
 
 class player:
 	def __init__(self,name='',desc='',inventory=None,location=None):
 		self.name=name
 		self.desc=desc
 		if inventory==None:
-			self.inventory=[]
+			self.inventory={}
 		self.location=location
 
 	def lookTo(self,door):
@@ -43,12 +63,32 @@ class player:
 			return self.location.doors[door].lookAt()
 		else:
 			return -1
+
 	def takeDoor(self,door):
 		if door in self.location.doors:
 			self.location=self.location.doors[door]
 			return 0
 		else:
 			return -1
+
+	def takeItem(self,item):
+		if item in self.location.objects:
+			self.intentory[item] = self.location.objects.pop[item]
+
+		else return -1
+
+	def use(self,thing,on):
+		if thing not in self.inventory.keys() or on not in self.inventory.keys():
+			return -2
+
+		if thing not int self.inventory[on].uses.keys():
+			return -1
+
+		self.inventory[on]=self.inventory[on].uses[thing]
+		if self.inventory[thing].isConsumable:
+			return self.inventory.pop(thing)
+
+		return 0
 
 
 
